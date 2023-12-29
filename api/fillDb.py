b1364f77c177b293cfad26dd708332c4c345bb07
bryan poleunis <bpoleunis@gmail.com>
2023-12-29 16:04:35 +0100

    changed fillDB

--- api/fillDb.py
+++ api/fillDb.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database import Base
-from models import Kerstmarkt, Kerstgerecht, Kerstdecoratie
+from models import Kerstmarkt, Kerstgerecht, Kerstdecoratie, User
 
 # Define your database URL (change it if your database is in a different location)
 DATABASE_URL = "sqlite:///./sqlitedb/kerst.db"
@@ -26,7 +26,7 @@ sample_gerecht_2 = Kerstgerecht(naam="Kerstgerecht 2", beschrijving="Beschrijvin
 sample_decoratie_1 = Kerstdecoratie(naam="Kerstdecoratie 1", type="Type 1", prijs=9.99)
 sample_decoratie_2 = Kerstdecoratie(naam="Kerstdecoratie 2", type="Type 2", prijs=14.75)
 
-# Add data to the session and commit
+# Add data to the session
 db.add(sample_markt_1)
 db.add(sample_markt_2)
 db.add(sample_gerecht_1)
@@ -34,6 +34,11 @@ db.add(sample_gerecht_2)
 db.add(sample_decoratie_1)
 db.add(sample_decoratie_2)
 
+# Add user admin
+admin_user = User(username="admin", email="admin@example.com")
+db.add(admin_user)
+
+# Commit the changes
 db.commit()
 
 # Close the session
