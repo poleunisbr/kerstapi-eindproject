3c3aff8490237e671f854d077a0498bf566ae936
bryan poleunis <bpoleunis@gmail.com>
2023-12-29 16:22:23 +0100

    updated database

--- api/database.py
+++ api/database.py
@@ -74,12 +74,6 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, index=True)
-    email = Column(String, unique=True, index=True)
 
 if __name__ == "__main__":
     create_database()
--- api/models.py
+++ api/models.py
@@ -26,7 +26,6 @@ class Kerstdecoratie(Base):
 
 class User(Base):
     __tablename__ = "users"
-
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, index=True)
     email = Column(String, index=True)
